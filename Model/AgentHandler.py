7f689e9ee54ab38aa77adeb84c34ab4eba025774
“mbt1” <mbt1@users.noreply.github.com>
2024-01-17 03:00:06 +0000

    .....

--- Model/AgentHandler.py
+++ Model/AgentHandler.py
@@ -19,15 +19,22 @@ class AgentHandler:
     def next_action(self, opponent_action):
         if opponent_action is not None:
             action_step=policy_step.PolicyStep(opponent_action)
-            self._time_step = self._exec_step(self._time_step,action_step,own_turn=False,self._buffer)
+            self._time_step = self._exec_step(action_step,own_turn=False)
+
+        action_step=self._agent.collect_policy.action(self._time_step)
+        next_time_step = self._exec_step(action_step,own_turn=True)
+        self._append_to_buffer(self._time_step,action_step,next_time_step)
+        self._time_step = next_time_step
+
+    def learn(self):
+        return "todo"/0
     
-    def _exec_step(self,time_step,action_step,own_turn,buffer):
+    def _append_to_buffer(self,time_step,action_step,next_time_step):
+        self._buffer.append(trajectory.from_transition(time_step, action_step, next_time_step))
+    
+    def _exec_step(self,action_step,own_turn):
         self._environment.set_next_is_agent_or_opponent(is_agent = own_turn)
-        next_time_step = self._environment.tf.step(action_step.action)
-        if own_turn:
-            step_trajectory = trajectory.from_transition(time_step, action_step, next_time_step)
-            buffer.append(step_trajectory)
-        return next_time_step
+        return self._environment.tf.step(action_step.action)
 
     def setup_agent(self, fc_layer_params, learning_rate, observation_spec, action_spec, time_step_spec):
         q_net = q_network.QNetwork(
